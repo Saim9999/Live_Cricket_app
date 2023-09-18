d882877057fe064a49865b69a58583f775039a0c
SaimHassan <saimhasan333@gmail.com>
2023-09-18 22:15:19 +0500

    commit

--- lib/screens/scorecard_rough.dart
+++ lib/screens/scorecard_rough.dart
@@ -24,6 +24,28 @@ class SquadItem {
   });
 }
 
+class ScorecardItem {
+  final String inningsTitle;
+  final String inningsInfo;
+  final String batterHeader;
+  final String runsHeader;
+  final String ballsHeader;
+  final String foursHeader;
+  final String sixesHeader;
+  final String strikeRateHeader;
+
+  ScorecardItem({
+    required this.inningsTitle,
+    required this.inningsInfo,
+    required this.batterHeader,
+    required this.runsHeader,
+    required this.ballsHeader,
+    required this.foursHeader,
+    required this.sixesHeader,
+    required this.strikeRateHeader,
+  });
+}
+
 class ScorecardRough extends StatefulWidget {
   final String url;
   const ScorecardRough({super.key, required this.url});
@@ -35,42 +57,13 @@ class ScorecardRough extends StatefulWidget {
 class _ScorecardRoughState extends State<ScorecardRough> {
   final List<MatchItem> matchItems = [];
   final List<SquadItem> squadItems = [];
+  final List<ScorecardItem> scorecardItems = [];
 
   @override
   void initState() {
     super.initState();
     scorecardMatches();
   }
-
-  // Future<void> scorecardMatches() async {
-  //   final response = await http.get(
-  //     Uri.parse(widget.url),
-  //   );
-  //   if (response.statusCode == 200) {
-  //     final document = html.parse(response.body);
-
-  //     final matchCardElements = document
-  //         .querySelectorAll('.cb-col.cb-col-67.cb-scrd-lft-col.html-refresh');
-
-  //     var matchInfoElements =
-  //         document.querySelectorAll('.cb-col.cb-col-100.cb-mtch-info-itm');
-
-  //     for (var matchInfoElement in matchInfoElements) {
-  //       var label = matchInfoElement.querySelector('.cb-col-27')?.text;
-  //       var value = matchInfoElement.querySelector('.cb-col-73')?.text;
-
-  //       if (label != null && value != null) {
-  //         print("$label: $value");
-  //         // print("$squad");
-  //       }
-  //       matchItems.add(MatchItem(matchLabel: label!, matchvalue: value!));
-  //     }
-
-  //     setState(() {});
-  //   } else {
-  //     print('Failed to fetch matches: ${response.statusCode}');
-  //   }
-  // }
 
   Future<void> scorecardMatches() async {
     final response = await http.get(
@@ -118,30 +111,68 @@ class _ScorecardRoughState extends State<ScorecardRough> {
         }
       }
 
-      final squadElements = document.querySelectorAll('.cb-minfo-tm-nm');
-      for (var squadElement in squadElements) {
-        final squadLabel = squadElement.text;
-        print(squadLabel);
-        squadItems.add(SquadItem(squadLabel: squadLabel));
+      final innings1Elements = document.querySelectorAll('#innings_1');
+      for (var innings1Element in innings1Elements) {
+        final inningsTitle =
+            innings1Element.querySelector('.cb-scrd-hdr-rw span');
+        final inningsInfo =
+            innings1Element.querySelector('.cb-scrd-hdr-rw span.pull-right');
+
+        final headerRow = document
+            .querySelector('.cb-col.cb-col-100.cb-scrd-sub-hdr.cb-bg-gray');
+        final headerColumns = headerRow!.querySelectorAll('.cb-col');
+
+        final batterHeader = headerColumns[0].text;
+        final runsHeader = headerColumns[2].text;
+        final ballsHeader = headerColumns[3].text;
+        final foursHeader = headerColumns[4].text;
+        final sixesHeader = headerColumns[5].text;
+        final strikeRateHeader = headerColumns[6].text;
+
+        print('Innings Title: ${inningsTitle!.text}');
+        print('Innings Info: ${inningsInfo!.text}');
+
+        print('Batter Header: $batterHeader');
+        print('Runs Header: $runsHeader');
+        print('Balls Header: $ballsHeader');
+        print('Fours Header: $foursHeader');
+        print('Sixes Header: $sixesHeader');
+        print('Strike Rate Header: $strikeRateHeader');
+
+        final headerRow1 =
+            document.querySelector('.cb-col.cb-col-100.cb-scrd-itms');
+        final headerColumns1 = headerRow1!.querySelectorAll('.cb-col');
+
+        final batterHeader1 = headerColumns1[0].text;
+        final runsHeader1 = headerColumns1[2].text;
+        final ballsHeader1 = headerColumns1[3].text;
+        final foursHeader1 = headerColumns1[4].text;
+        final sixesHeader1 = headerColumns1[5].text;
+        final strikeRateHeader1 = headerColumns1[6].text;
+
+        print('Batter Header1: $batterHeader1');
+        print('Runs Header1: $runsHeader1');
+        print('Balls Header1: $ballsHeader1');
+        print('Fours Header1: $foursHeader1');
+        print('Sixes Header1: $sixesHeader1');
+        print('Strike Rate Header1: $strikeRateHeader1');
+
+        scorecardItems.add(ScorecardItem(
+            inningsTitle: inningsTitle.text,
+            inningsInfo: inningsInfo.text,
+            batterHeader: batterHeader1,
+            ballsHeader: ballsHeader1,
+            runsHeader: runsHeader1,
+            foursHeader: foursHeader1,
+            sixesHeader: sixesHeader1,
+            strikeRateHeader: strikeRateHeader1));
       }
 
-      // if (squadElement != null) {
+      // final squadElements = document.querySelectorAll('.cb-minfo-tm-nm');
+      // for (var squadElement in squadElements) {
       //   final squadLabel = squadElement.text;
-      //   final playerLinks =
-      //       squadElement.nextElementSibling?.querySelectorAll('a');
-
-      //   if (squadLabel.isNotEmpty &&
-      //       playerLinks != null &&
-      //       playerLinks.isNotEmpty) {
-      //     final squadPlayers = playerLinks.map((playerLink) {
-      //       final playerName = playerLink.text;
-      //       final playerProfileUrl = playerLink.attributes['href'];
-      //       return '$playerName ($playerProfileUrl)';
-      //     }).join(', ');
-
-      //     squadItems
-      //         .add(SquadItem(squadLabel: squadLabel, squadvalue: squadPlayers));
-      //   }
+      //   print(squadLabel);
+      //   squadItems.add(SquadItem(squadLabel: squadLabel));
       // }
 
       setState(() {});
@@ -199,6 +230,37 @@ class _ScorecardRoughState extends State<ScorecardRough> {
                     height: 10.h,
                   ),
                   Text('${squadItem.squadLabel}'),
+                ],
+              );
+            },
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            itemCount: scorecardItems.length,
+            itemBuilder: (context, index) {
+              final scorecardItem = scorecardItems[index];
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text('Innings Title : ${scorecardItem.inningsTitle}'),
+                  SizedBox(
+                    height: 5.h,
+                  ),
+                  Text('Innings Info : ${scorecardItem.inningsInfo}'),
+                  SizedBox(
+                    height: 5.h,
+                  ),
+                  Text('Batter Header1:      ${scorecardItem.batterHeader}'),
+                  Text('Runs Header1:        ${scorecardItem.runsHeader}'),
+                  Text('Balls Header1:       ${scorecardItem.ballsHeader}'),
+                  Text('Fours Header1:       ${scorecardItem.foursHeader}'),
+                  Text('Sixes Header1:       ${scorecardItem.sixesHeader}'),
+                  Text(
+                      'Strike Rate Header1: ${scorecardItem.strikeRateHeader}'),
                 ],
               );
             },
